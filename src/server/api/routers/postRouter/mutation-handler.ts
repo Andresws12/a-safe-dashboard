4730f8e9aa6a3790a73e949836e3d1be217e44ba
Andres Hernandez <andreshernandez12.1995@gmail.com>
2025-03-16 13:14:37 +0100

    Fix mutation handler

--- src/server/api/routers/postRouter/mutation-handler.ts
+++ src/server/api/routers/postRouter/mutation-handler.ts
@@ -2,11 +2,42 @@ import { prisma } from '~/server/prisma';
 import { defaultPostSelect } from './postHelpers';
 import { addPostSchema } from '~/server/schemas/postSchemas';
 import { z } from 'zod';
+import { TRPCError } from '@trpc/server';
 
 export const addPost = async (input: z.infer<typeof addPostSchema>) => {
-  const post = await prisma.post.create({
-    data: input,
-    select: defaultPostSelect,
-  });
-  return post;
+  try {
+    const validatedData = addPostSchema.parse(input);
+
+    const postData = {
+      ...validatedData,
+      date: validatedData.date || new Date().toISOString().split('T')[0],
+      readTime: validatedData.readTime || '5 min',
+      author: validatedData.author || 'Anonymous',
+    };
+
+    if ('id' in postData) {
+      delete postData.id;
+    }
+
+    const post = await prisma.post.create({
+      data: postData,
+      select: defaultPostSelect,
+    });
+
+    return post;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: `Datos de post inválidos: ${error.message}`,
+        cause: error,
+      });
+    }
+
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'No se pudo crear el post',
+      cause: error,
+    });
+  }
 };
